2fafb921ee846b468d691fb4532835c4b9aac2cb
Michelle Fonseca <michelle.fonseca@flecharoja.com>
2026-03-12 12:00:21 -0600

    se agrega CRUD de contenedores y se cambia el orderBy en el service debido a un error de campo inexistente

--- costly-backend/src/app.js
+++ costly-backend/src/app.js
@@ -18,7 +18,7 @@ import authRoutes from './modules/auth/auth.routes.js'
 // import costeosRoutes from './modules/costeos/costeos.routes.js'
 // import pagosRoutes from './modules/pagos/pagos.routes.js'
 // import hitosRoutes from './modules/hitos/hitos.routes.js'
-// import contenedoresRoutes from './modules/contenedores/contenedores.routes.js'
+import contenedoresRoutes from './modules/contenedores/contenedores.routes.js'
 // import tramiteRoutes from './modules/tramite-aduana/tramite.routes.js'
 // import tcRoutes from './modules/tc-historico/tc.routes.js'
 // import documentosRoutes from './modules/documentos/documentos.routes.js'
@@ -89,7 +89,7 @@ app.use(`${API}/auth`, authRoutes)
 // app.use(`${API}/costeos`,        costeosRoutes)
 // app.use(`${API}/pagos`,          pagosRoutes)
 // app.use(`${API}/hitos`,          hitosRoutes)
-// app.use(`${API}/contenedores`,   contenedoresRoutes)
+app.use(`${API}/contenedores`, contenedoresRoutes)
 // app.use(`${API}/tramite-aduana`, tramiteRoutes)
 // app.use(`${API}/tc`,             tcRoutes)
 // app.use(`${API}/documentos`,     documentosRoutes)
