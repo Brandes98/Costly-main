e77a98d0df60bd782e14ba4c8ce19d2c5cc1cb5b
Michelle Fonseca <michelle.fonseca@flecharoja.com>
2026-03-13 14:30:35 -0600

    se agrega CRUD de pagos

--- costly-backend/src/app.js
+++ costly-backend/src/app.js
@@ -16,7 +16,7 @@ import authRoutes from './modules/auth/auth.routes.js'
 // import pedidosRoutes from './modules/pedidos/pedidos.routes.js'
 // import importacionesRoutes from './modules/importaciones/importaciones.routes.js'
 // import costeosRoutes from './modules/costeos/costeos.routes.js'
-// import pagosRoutes from './modules/pagos/pagos.routes.js'
+import pagosRoutes from './modules/pagos/pagos.routes.js'
 // import hitosRoutes from './modules/hitos/hitos.routes.js'
 // import contenedoresRoutes from './modules/contenedores/contenedores.routes.js'
 // import tramiteRoutes from './modules/tramite-aduana/tramite.routes.js'
@@ -87,7 +87,7 @@ app.use(`${API}/auth`, authRoutes)
 // app.use(`${API}/pedidos`,        pedidosRoutes)
 // app.use(`${API}/importaciones`,  importacionesRoutes)
 // app.use(`${API}/costeos`,        costeosRoutes)
-// app.use(`${API}/pagos`,          pagosRoutes)
+app.use(`${API}/pagos`, pagosRoutes)
 // app.use(`${API}/hitos`,          hitosRoutes)
 // app.use(`${API}/contenedores`,   contenedoresRoutes)
 // app.use(`${API}/tramite-aduana`, tramiteRoutes)
